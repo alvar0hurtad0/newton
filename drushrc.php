e28eeae3346cb32a38ce9f6530aea7e45be1a30e
alvar0hurtad0 <alvar0hurtad0@1564318.no-reply.drupal.org>
2014-02-27 22:32:52 +0000

    =drush capasao added

--- drushrc.php
+++ drushrc.php
@@ -1,3 +1,5 @@
 <?php
 $options['shell-aliases']['pull'] = '!git pull';
 $options['shell-aliases']['push'] = '!git pull && git push';
+$options['shell-aliases']['traepaca'] = '!drush sql-sync @default.pro default && drush rsync @default.pro:sites/default/files default:sites/default/files && drush cc all && drush vset preprocess_css 0 --yes';
+
