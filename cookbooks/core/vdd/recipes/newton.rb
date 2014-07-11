1b491d7d30fdceda2d0d98750ca73208d6f60bd3
Alvaro Hurtado <alvaro@dsmultimedia.es>
2014-07-11 21:20:50 +0200

    newton fixed

--- cookbooks/core/vdd/recipes/newton.rb
+++ cookbooks/core/vdd/recipes/newton.rb
@@ -1,3 +1,17 @@
+directory "/home/vagrant/.drush/newton" do
+  owner "vagrant"
+  group "vagrant"
+  mode "0755"
+  action :create
+end
+
+directory "/home/vagrant/.drush/newton" do
+  owner "vagrant"
+  group "vagrant"
+  mode "0755"
+  action :create
+end
+
 git "/home/vagrant/.drush/newton" do
   repository "https://github.com/alvar0hurtad0/newton.git"
   revision "master"
